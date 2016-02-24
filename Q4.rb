ef261a2d9afd29059ad675ff28cac3b77a340116
hprrkn <prrknh@gmail.com>
2016-02-24 16:14:17 +0000

    (WIP)Q4

--- Q4.rb
+++ Q4.rb
@@ -2,14 +2,17 @@ block=1
 n=20
 m=3
 
-for i in 1..100
-	if block == n
+for i in 1..20
+	if block >= n
 		p i
 		break
-	elseif block < n
-		block* m
-	elseif block < m
-		block * 2
+	elseif block < m 
+		block = block * 2
+		p " * 2"
 	else
+		block = block * m
+		p " * m"
 	end
+	p i
+	p block
 end
