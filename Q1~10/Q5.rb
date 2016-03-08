67f196c6520dce9f0a8201f66695319da7cc5da0
hprrkn <prrknh@gmail.com>
2016-03-09 01:07:04 +0900

    Q5 use each method

--- Q1~10/Q5.rb
+++ Q1~10/Q5.rb
@@ -4,25 +4,21 @@ AMOUNT50 = 50
 AMOUNT10 = 10
 
 def method(i,m)
-    patternCnt = 0
-    for cnt500 in 0..i/AMOUNT500 do
-        for cnt100 in 0..i/AMOUNT100 do
-            for cnt50 in 0..i/AMOUNT50 do
-                for cnt10 in 0..i/AMOUNT10 do
+	patternCnt = 0
+	(0..i/AMOUNT500).each{|cnt500|
+		(0..i/AMOUNT100).each{|cnt100|
+			(0..i/AMOUNT50).each{|cnt50|
+				(0..i/AMOUNT10).each{|cnt10|
                     if cnt500 + cnt100 + cnt50 + cnt10 <= m
                         if cnt500*AMOUNT500 + cnt100*AMOUNT100 + cnt50*AMOUNT50 + cnt10*AMOUNT10 == i
                             patternCnt += 1
                         end
-                    end
-                    cnt10 += 1
-                end
-                cnt50 += 1
-            end
-            cnt100 += 1
-        end
-        cnt500 += 1
-    end
-    return patternCnt
+					end
+				}
+			}
+		}
+	}
+	return patternCnt
 end
 
 p method(1000,15)
