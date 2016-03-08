dd52444effda9bacb33117b1ab67d8779107031b
hprrkn <prrknh@gmail.com>
2016-03-08 23:32:59 +0900

    Q6 use ternary_operator

--- Q1~10/Q6.rb
+++ Q1~10/Q6.rb
@@ -3,11 +3,7 @@ cnt = 0
 while i < 10000 do
 	n = i*3 + 1
 	while n != 1 do
-		if n%2 == 0 then
-			n = n/2
-		else
-			n = n*3 + 1
-		end
+		n = n%2 == 0 ? n/2 : n*3 + 1
 		if n == i then
 			cnt += 1
 		end
